de0553f76bb37202fedc9c8b1c81ed7fc5dbb103
Colton-Grant <124389241+Colton-Grant@users.noreply.github.com>
2023-06-20 16:21:43 +0000

    up

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: colton
+// Created on: june 2023
 // This file contains the JS functions for index.html
+
+
+
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-6-02/sw.js", {
+    scope: "/ICS2O-6-02/",
+  })
+}
+function cookiePoint() {
+  if (localStorage.howManyTimesClickedTheCookie) {
+    localStorage.howManyTimesClickedTheCookie =
+      Number(localStorage.howManyTimesClickedTheCookie) + 1
+  } else {
+    localStorage.howManyTimesClickedTheCookie = 1
+  }
+  document.getElementById("hits").innerHTML =
+    "Total Hits :" + localStorage.howManyTimesClickedTheCookie
+}
